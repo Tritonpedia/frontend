631628ffeb540d25ebb2a8878ac1a13e37be9772
Anthony Tarbinian <atar137h@gmail.com>
2021-11-06 23:01:19 -0700

    Working create class functionality if department doesn't exist
    need to clean up reused code more

--- client/src/homeComponents/createPageClass.js
+++ client/src/homeComponents/createPageClass.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./createPage.css";
 //import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
-function createPageClass(){
+function createPageClass() {
 
     let coverImageInput;
 
@@ -11,8 +11,8 @@ function createPageClass(){
         const reader = new FileReader();
         reader.addEventListener("load", function () {
             // convert image file to base64 string
-              preview.src = reader.result;
-              coverImageInput = reader.result;
+            preview.src = reader.result;
+            coverImageInput = reader.result;
             //   console.log(reader.result);
             // setPreviewImgSrc(reader.result);
         }, false);
@@ -22,33 +22,33 @@ function createPageClass(){
         }
     }
 
-    
-    function handleSubmit(e){
+
+    function handleSubmit(e) {
         e.preventDefault();
 
         let titleInput;
-        let typeInput; 
+        let typeInput;
         let contentInput;
-        
-        if(document.getElementById("titleInput").value === null){
+
+        if (document.getElementById("titleInput").value === null) {
             titleInput = " ";
         }
-        else{
-            titleInput = document.getElementById("titleInput");    
-        }
-        
-        if(document.getElementById("typeInput").value === null){
-            typeInput = " ";
-        }
-        else{
-            typeInput = document.getElementById("typeInput");    
+        else {
+            titleInput = document.getElementById("titleInput");
         }
 
-        if(document.getElementById("contentInput").value === null){
+        if (document.getElementById("typeInput").value === null) {
+            typeInput = " ";
+        }
+        else {
+            typeInput = document.getElementById("typeInput");
+        }
+
+        if (document.getElementById("contentInput").value === null) {
             contentInput = " ";
         }
-        else{
-            contentInput = document.getElementById("contentInput");    
+        else {
+            contentInput = document.getElementById("contentInput");
         }
 
         let user = JSON.parse(localStorage.getItem('currentSession'));
@@ -63,46 +63,78 @@ function createPageClass(){
         }
 
         console.log(postBody);
-
-        fetch(`http://localhost:5000/class/${typeInput.value}`, {
-            method: 'POST', 
+        fetch(`http://localhost:5000/department/${typeInput.value}`, {
+            method: 'GET',
             headers: {
                 'content-type': "application/json",
                 'auth_token': JWTtoken
-            },
-            body: JSON.stringify(postBody) 
-        }).then(response => {window.location.assign("/successPage")});
-     //}).then(response => console.log(response));
+            }
+        }).then(response => {
+            if (!response.json()["value"]) {
+                fetch(`http://localhost:5000/department/`, {
+                    method: 'POST',
+                    headers: {
+                        'content-type': "application/json",
+                        'auth_token': JWTtoken
+                    },
+                    body: JSON.stringify({
+                        name: typeInput.value
+                    })
+                }).then(() => {
+                    fetch(`http://localhost:5000/class/${typeInput.value
+                        }`, {
+                        method: 'POST',
+                        headers: {
+                            'content-type': "application/json",
+                            'auth_token': JWTtoken
+                        },
+                        body: JSON.stringify(postBody)
+                    }).then(response => { window.location.assign("/successPage") });
+
+                });
+            } else {
+                fetch(`http://localhost:5000/class/${typeInput.value}`, {
+                    method: 'POST',
+                    headers: {
+                        'content-type': "application/json",
+                        'auth_token': JWTtoken
+                    },
+                    body: JSON.stringify(postBody)
+                    }).then(response => {window.location.assign("/successPage")});
+                // }).then(response => console.log(response));
+            }
+        })
+
     }
 
-    return(
-        
-        <div className="spacing" style={{backgroundImage: "url(https://ucsandiegobookstore.com/Images/06BookstorePerksWallpapers.png)", backgroundRepeat: "no-repeat", backgroundPosition: "center"}}>
+    return (
+
+        <div className="spacing" style={{ backgroundImage: "url(https://ucsandiegobookstore.com/Images/06BookstorePerksWallpapers.png)", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}>
             <div className="fullPage">
                 <div id="buttonsDiv">
                     <a href="/">
-                        <button style={{backgroundColor: "transparent", border: "none"}}>
+                        <button style={{ backgroundColor: "transparent", border: "none" }}>
                             Back<i className='fas fa-arrow-left'></i>
                         </button>
                     </a>
 
                     <a href="/createPageSection">
-                    <button className="createsection-button">
-                        Create Section
-                    </button>
+                        <button className="createsection-button">
+                            Create Section
+                        </button>
                     </a>
                 </div>
 
                 <div id="formDiv" className="spacing">
-                    <form onSubmit={e => handleSubmit(e)} style={{display: "flex", flexDirection: "column", justifyContent: "flex-start"}}>
+                    <form onSubmit={e => handleSubmit(e)} style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start" }}>
                         <label className="spacing yellow">Department</label>
-                        <input id="typeInput" className="textbox " style={{type: "text", height: "30px", fontSize: "18px", fontFamily: "Montserrat, sans-serif", zIndex: "5"}} required></input>
+                        <input id="typeInput" className="textbox " style={{ type: "text", height: "30px", fontSize: "18px", fontFamily: "Montserrat, sans-serif", zIndex: "5" }} required></input>
 
                         <label id="largetextbox" className="spacing yellow">Course Name</label>
-                        <input id="titleInput" className="textbox" style={{type: "text", height: "30px", fontSize: "18px", fontFamily: "Montserrat, sans-serif", zIndex: "5"}} required></input>
+                        <input id="titleInput" className="textbox" style={{ type: "text", height: "30px", fontSize: "18px", fontFamily: "Montserrat, sans-serif", zIndex: "5" }} required></input>
 
                         <label className="spacing yellow">Content</label>
-                        <textarea id="contentInput" class="textbox" style={{height: "392px", fontSize: "18px", fontFamily: "Montserrat, sans-serif", zIndex: "5"}} required/>
+                        <textarea id="contentInput" class="textbox" style={{ height: "392px", fontSize: "18px", fontFamily: "Montserrat, sans-serif", zIndex: "5" }} required />
 
                         <label class="spacing yellow">Cover Photo</label>
                         <input id="coverImageInput" className="yellow" type="file" name="filename" onChange={previewImage}></input>
@@ -112,12 +144,12 @@ function createPageClass(){
                         {/* <label className="spacing yellow">Additional Links</label>
                         <input id="linksInput" class="textbox" style={{type: "text", height: "30px", fontSize: "18px", fontFamily: "Montserrat, sans-serif", zIndex: "5"}}></input> */}
 
-                        <div className="spacing" style={{float: "right", marginTop: "3%"}}>
+                        <div className="spacing" style={{ float: "right", marginTop: "3%" }}>
                             <button type="submit" className="publish-button">
                                 Publish
                             </button>
                         </div>
-                        
+
                     </form>
 
 
